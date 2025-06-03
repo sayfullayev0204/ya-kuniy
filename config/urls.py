fa80d41641f0e6b0f17358407e64fa7a0354c702
sayfullayev0204 <sayfullayevgayrat7@gmail.com>
2025-06-03 13:20:48 +0500

    a

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,re_path,include
+from django.views.generic import TemplateView
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
